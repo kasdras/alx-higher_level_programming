2c5117d5c5d776349056d5c07820cc3685856326
Idris Kazeem <kazdraz0@gmail.com>
2022-12-19 07:33:26 -0800

    The last digit and sign recognition

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,17 +2,22 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10
+str = "Last digit of {} is {} and is"
+str1 = "greater than 5"
+str3 = "0"
+str4 = "less than 6 and not 0"
 if number < 0:
     positiveNum = number * -1
     change = positiveNum % 10
     if change == 0:
-        print("Last digit of {} is {} and is 0".format(number, change))
+        print(str.format(number, change), str3)
     else:
-        print("Last digit of {} is -{} and is less than 6 and not 0".format(number, change))
+        str = "Last digit of {} is -{} and is"
+        print(str.format(number, change), str4)
 else:
     if lastDigit > 5:
-        print("Last digit of {} is {} and is greater than 5".format(number, lastDigit))
+        print(str.format(number, lastDigit), str1)
     elif lastDigit < 6 and lastDigit != 0:
-        print("Last digit of {} is {} and is less than 6 and not 0".format(number, lastDigit))
+        print(str.format(number, lastDigit), str4)
     else:
-        print("Last digit of {} is {} and is 0".format(number, lastDigit))
+        print(str.format(number, lastDigit), str3)
