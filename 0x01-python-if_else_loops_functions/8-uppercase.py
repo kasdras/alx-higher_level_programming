3afc557e94841278f45609332eabcbcf07a30a22
Idris Kazeem <kazdraz0@gmail.com>
2022-12-21 02:10:15 -0800

    function prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uppercase(str):
+    alp = 'abcdefghijklmnopqrstuvwxyz'
+    # loops through the parameter str
+    for char in str:
+        # loops through the alphabets
+        if char in alp:
+            uniChar = ord(char) # returns the unicode value of the character
+            charValue = uniChar - 32
+            output = output+chr(charValue)
+        else:
+            output = output+char
+    return output
