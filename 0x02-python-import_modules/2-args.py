977a78d18dd2d575e5b86425c740c20392d29dec
Idris Kazeem <kazdraz0@gmail.com>
2023-01-02 06:16:15 -0800

    altered the formating strings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv[1:]
-n = len(argv)
-i = 1
-if n == 0:
-    print("{} arguments.".format(n))
-elif n == 1:
-    print("{} argument:".format(n))
-    print("{}: {}".format(i, argv[1]))
-else:
-    print("{} arguments:".format(n))
-    while i <= n:
-        print("{}: {}".format(i, argv[i]))
-        i += 1
+if __name__ = "__main__":
+    import sys
+    argv = sys.argv[1:]
+    n = len(argv)
+    i = 1
+    if n == 0:
+        print("{:d} arguments.".format(n))
+    elif n == 1:
+        print("{:d} argument:".format(n))
+        print("{:d}: {:s}".format(i, argv[1]))
+    else:
+        print("{:d} arguments:".format(n))
+        while i <= n:
+            print("{:d}: {:s}".format(i, argv[i]))
+            i += 1
