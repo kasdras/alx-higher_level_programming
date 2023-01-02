cffc72010acf1ab263f34942af5fab3a92e3b2f9
Idris Kazeem <kazdraz0@gmail.com>
2023-01-02 06:11:00 -0800

    removed the if statement at the beginning

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    n = len(argv)
-    i = 1
-    if n == 0:
-        print("{} arguments.".format(n))
-    elif n == 1:
-        print("{} argument:".format(n))
-        print("{}: {}".format(i, sys.argv[1]))
-    else:
-        print("{} arguments:".format(n))
-        while i <= n:
-            print("{}: {}".format(i, sys.argv[i]))
-            i += 1
+import sys
+argv = sys.argv[1:]
+n = len(argv)
+i = 1
+if n == 0:
+    print("{} arguments.".format(n))
+elif n == 1:
+    print("{} argument:".format(n))
+    print("{}: {}".format(i, argv[1]))
+else:
+    print("{} arguments:".format(n))
+    while i <= n:
+        print("{}: {}".format(i, argv[i]))
+        i += 1
