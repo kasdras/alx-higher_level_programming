0db8fd7192500d71cc06ecb4f9cacfd9bd3e92d5
Idris Kazeem <kazdraz0@gmail.com>
2023-01-02 05:00:50 -0800

    n was not properly defined

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = len(argv)
+    n = len(sys.argv)
     i = 1
     if n == 1:
-        print("{} argument.".format(0))
+        print("{} arguments.".format(0))
     elif n == 2:
         print("{} argument:".format(i))
         print("{}: {}".format(i, sys.argv[i]))
