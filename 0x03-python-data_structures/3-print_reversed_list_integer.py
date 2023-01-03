08c8c2b048da2bf129488e52dde955acd7e184ee
Idris Kazeem <kazdraz0@gmail.com>
2023-01-03 13:49:53 -0800

    reassign the reversed integer to a new variable

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    rev_list = []
     for i in range(len(my_list) - 1, my_list[0], -1):
+        rev_list = i
+    for i in rev_list:
         print("{}".format(i))
-        return my_list
