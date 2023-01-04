8d66690003db4f4d4af81c1872a854dd5415878c
Idris Kazeem <kazdraz0@gmail.com>
2023-01-04 01:14:11 -0800

    used half of the length of the array to reverse the integers

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list =  my_list[::-1]
-    for i in new_list:
+    #new_list =  my_list[::-1] //another way of reversing a list
+    i = 0
+    while i < len(my_list) / 2:
+        temp = my_list[i]
+        my_list[i] = my_list[len(my_list) - i - 1]
+        my_list[len(my_list) - i - 1] = temp
+        i += 1
+    for i in my_list:
         print("{:d}".format(i))
